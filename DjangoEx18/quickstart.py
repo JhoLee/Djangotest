28f802b2774be341663b60aced26faa810be1607
haeun <aimeeha@naver.com>
2018-11-05 21:20:55 +0900

    update google 켈린더 접근을 위한 api 사용 test 오타수정

--- DjangoEx18/quickstart.py
+++ DjangoEx18/quickstart.py
@@ -16,8 +16,8 @@
 from __future__ import print_function
 import datetime
 from googleapiclient.discovery import build
-from httplib2 import http
-from oauth2client import file,client, tools
+from httplib2 import Http
+from oauth2client import file, client, tools
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
@@ -31,7 +31,7 @@ def main():
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
-    service = build('calendar', 'v3', http=creds.authorize(http()))
+    service = build('calendar', 'v3', http=creds.authorize(Http()))
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
